92e4a35115fb15230b4f25d7b4816f005aff1b69
kakubotics <yashkhandelwal27@gmail.com>
2020-05-15 18:34:19 +0530

    First

--- App.js
+++ App.js
@@ -6,33 +6,17 @@ import reducer from './reducers';
 
 
 import History from './components/History'
+import TabNav from './components/TabNav'
 
-<<<<<<< HEAD
 export default class App extends React.Component {
   render() {
     return (
       <Provider store={createStore(reducer)}>
         <View style={{ flex: 1}}>
           <View style={{height:20}}/>
-          <History/>
+          <TabNav/>
         </View>
       </Provider>
     );
   }
 }
-=======
-  render () {
-      return (
-        <Provider store={{flex:1}}>
-          <View style={styles.container}>
-            <History/>
-          </View>
-        </Provider>
-          
-      )
-  }
-}
-
-
-
->>>>>>> 9d5cf04162641d4b454711d55734c482c9e29f86
